f65ed261aaf289e96f53efd6baa1bbc99f69f62e
Katelyn Curtiss <153852200+KatelynCurtiss@users.noreply.github.com>
2024-10-01 11:15:47 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+# Katelyn Curtiss
+# October 1 2024
+# If Else statements starter
+
+
+# Task 1
+#Completed reading pp. 79-80
+
+# Task 2
+is_green = False
+if is_green:
+    print("You just eraned 5 points for shooting the alien! ")
+else:
+    print("You just earned 10 points")
+
+
+# Task 3
+is_your_name  = (input("Your name?: "))
+if 0 > 5 : 
+    print("Your name is to short")
+
+else:
+    print("Your name is too long. ")    
+
+
+# Task 4
+is_letter = (input("A letter in the alphabet?: "))
+
+# Task 5 
+enter_int = (input("Enter 2 Intagers:" ))
